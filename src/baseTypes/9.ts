a827363f768c202ab104d69eb13d6935cc97acc6
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-05-15 20:01:32 +0300

    remind base rules

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,22 +2,34 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+type pages = {
+  title: string;
+  likes: number;
+  accounts: Array<string>;
+  status: "open" | "close";
+  details?: Details;
+};
+type Details = {
+  createAt: Date;
+  updateAt: Date;
+};
 
-const page2 = {
-  title: 'Python or Js',
+const page1: pages = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2: pages = {
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
